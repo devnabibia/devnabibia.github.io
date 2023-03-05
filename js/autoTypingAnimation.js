74d117cb4a2707856a059c5e8a4ac00d65b717eb
devnabbia <nangendodennis@gmail.com>
2023-03-05 09:57:36 +0300

    gggggggg

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Dennis Nangendo</span> Innovative Front End Developer with 5 years experience building and maintaining responsive websites in the recruiting industry. "
+        "<span>Hi, I am Dennis Nangendo</span> Frontend developer with proven experience in helping companies create and maintain a better code base for reusability. Capable of continuous learning from senior developers while still nurturing junior developers. "
             ],
       typeSpeed: 7,
       contentType: 'html'
